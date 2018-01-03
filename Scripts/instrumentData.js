500152bd45b2a47e9c01102fc91911010020ae77
d-healey <dave41264@hotmail.com>
2018-01-03 18:18:03 +0000

    Added display name property to instruments

--- Scripts/instrumentData.js
+++ Scripts/instrumentData.js
@@ -1,21 +1,22 @@
 namespace instData
 {	
-	const var allArticulations = ["normal", "staccato", "fingernail", "table", "harmonics"];
+	const var allArticulations = ["sustain", "legato", "staccato", "flutter", "harmonics"];
 	const var articulationDisplayNames = ["Normal", "Staccato", "Fingernail", "Prés de la table", "Harmonics"];
 	reg programs = [1, 40, 9, 17, 10]; //UACC and Program Change numbers for articulations
 
 	const var database = {
-		harp:
+		altoFlute:
 		{
-			range:[26, 96], //Maximum range of instrument
-			keyswitches:[98, 99, 100, 101, 102], //Default keyswitches (unused should be set to -1)
+		    displayName:"Alto Flute",
+			range:[60, 96], //Maximum range of instrument
+			keyswitches:[24, 25, 26, 27, 28], //Default keyswitches (unused should be set to -1)
 			articulations:
 			{
-				normal:{range:[26, 96]},
-				staccato:{range:[26, 96]},
-				fingernail:{range:[26, 96]},
-				table:{range:[26, 96]},
-				harmonics:{range:[40, 88]}
+				normal:{range:[60, 96]},
+				staccato:{range:[60, 96]},
+				fingernail:{range:[60, 96]},
+				table:{range:[60, 96]},
+				harmonics:{range:[60, 88]}
 			}
 		}		
 	};
