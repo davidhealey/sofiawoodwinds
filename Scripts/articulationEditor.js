1ec2fef162d84dbd2b9fe885dca4882409290c3a
d-healey <dave41264@hotmail.com>
2018-01-15 19:00:12 +0000

    Replaced many release samplers with a single sampler

--- Scripts/articulationEditor.js
+++ Scripts/articulationEditor.js
@@ -25,7 +25,7 @@ namespace articulationEditor
 		const var muterIds = Synth.getIdList("MidiMuter");
 		const var containerIds = Synth.getIdList("Container");
         const var rates = ["1/1", "1/2D", "1/2", "1/2T", "1/4D", "1/4", "1/4T", "1/8D", "1/8", "1/8T", "1/16D", "1/16", "1/16T", "1/32D", "1/32", "1/32T", "1/64D", "1/64", "1/64T", "Velocity"]; //Glide rates
-        const var releaseHandler = Synth.getMidiProcessor("sustainReleaseHandler"); //Sustain/legato/glide release handler
+        const var releaseHandler = Synth.getMidiProcessor("releaseHandler"); //Release handler
         
 		reg containers = []; //Containers whose IDs match articulation names
 		reg muters = [];
