dc54ac90c96f5d500052557717f49f4896f6d95d
d-healey <dave41264@hotmail.com>
2018-03-18 22:33:42 +0000

    Removed generic mixer, main interface mixer now works directly with simple gain modules

--- Scripts/mixer.js
+++ Scripts/mixer.js
@@ -19,32 +19,29 @@ namespace mixer
 {
 	inline function onInitCB()
 	{
-		const var genericMixer = Synth.getMidiProcessor("genericMixer");
-		const var ATTRIBUTES = {GAIN:0, DELAY:1, WIDTH:2, PAN:3, MUTE:4, SOLO:5, PURGE:6}; //The attributes/controls of the mic mixer module script
-		const var NUM_ATTRIBUTES = 7;
+		
+        //Retrieve samplers and store in samplers array
+        const var samplerIds = Synth.getIdList("Sampler"); //Get IDs of samplers
+        const var samplers = [];
+
+        for (s in samplerIds)
+        {
+            samplers.push(Synth.getSampler(s));
+        }
 		
 		const var micNames = ["Close", "Decca", "Hall"]; //Close, decca, hall
 		const var pan = [];
-		const var vol = [];
-		const var delay = [];
-		const var width = [];
 		const var purge = [];
 
 		for (i = 0; i < micNames.length; i++)
 		{
-			pan[i] = Content.getComponent("sliPan"+i);
-			vol[i] = Content.getComponent("sliVol"+i);
-			delay[i] = Content.getComponent("sliDelay"+i);
-			width[i] = Content.getComponent("sliWidth"+i);
-			purge[i] = Content.getComponent("btnPurge"+i);
-
-			Content.setPropertiesFromJSON("sliPan"+i, {x:20+(i*60), stepSize:0.01, bgColour:Theme.CONTROL1, itemColour:Theme.CONTROL2});
-			Content.setPropertiesFromJSON("sliVol"+i, {x:35+(i*60), type:"Decibel", max:3, bgColour:Theme.CONTROL1, itemColour:Theme.CONTROL2});
-			Content.setPropertiesFromJSON("sliDelay"+i, {x:20+(i*60), bgColour:Theme.CONTROL1, itemColour:Theme.CONTROL2});
-			Content.setPropertiesFromJSON("sliWidth"+i, {x:20+(i*60), bgColour:Theme.CONTROL1, itemColour:Theme.CONTROL2});
-			Content.setPropertiesFromJSON("btnPurge"+i, {x:20+(i*60), text:micNames[i], bgColour:Theme.CONTROL1, textColour:Theme.BLACK});
-	
+			pan[i] = ui.setupControl("sliPan"+i, {stepSize:0.01, bgColour:Theme.CONTROL1, itemColour:Theme.CONTROL2});
 			ui.sliderPanel("sliPan"+i, paintRoutines.biDirectionalSlider, 0, 0.5); //Set up callbacks for pan slider
+
+			Content.setPropertiesFromJSON("sliVol"+i, {type:"Decibel", max:3, bgColour:Theme.CONTROL1, itemColour:Theme.CONTROL2});
+			Content.setPropertiesFromJSON("sliDelay"+i, {bgColour:Theme.CONTROL1, itemColour:Theme.CONTROL2});
+			Content.setPropertiesFromJSON("sliWidth"+i, {bgColour:Theme.CONTROL1, itemColour:Theme.CONTROL2});
+			purge[i] = ui.setupControl("btnPurge"+i, {text:micNames[i], bgColour:Theme.CONTROL1, textColour:Theme.BLACK});			
 			ui.buttonPanel("btnPurge"+i, paintRoutines.textButton); //Set up callbacks for purge button
 		}
     }
@@ -53,30 +50,21 @@ namespace mixer
 	{
 		for (i = 0; i < micNames.length; i++)
 		{
-			if (number == vol[i])
+			if (number == pan[i])
 			{
-				genericMixer.setAttribute(ATTRIBUTES.GAIN+1 * (NUM_ATTRIBUTES*i), value);
-				break;
-			}
-			else if (number == pan[i])
-			{
-				genericMixer.setAttribute(ATTRIBUTES.PAN+1 * (NUM_ATTRIBUTES*i), value);	
 				pan[i].repaint();
-				break;
-			}
-			else if (number == delay[i])
-			{
-				genericMixer.setAttribute(ATTRIBUTES.DELAY+1 * (NUM_ATTRIBUTES*i), value);
-				break;
-			}
-			else if (number == width[i])
-			{
-				genericMixer.setAttribute(ATTRIBUTES.WIDTH+1 * (NUM_ATTRIBUTES*i), value);
 				break;
 			}
 			else if (number == purge[i])
 			{
-				genericMixer.setAttribute(ATTRIBUTES.PURGE+1 * (NUM_ATTRIBUTES*i), 1-value);
+                for (s in samplers) //Each sampler
+                {
+                    //Only purge or load if it's not already purged/loaded
+                    if (s.getNumMicPositions() > 1 && s.isMicPositionPurged(i) != value)
+                    {
+                        s.purgeMicPosition(s.getMicPositionName(i), value);
+                    }
+                }
 				purge[i].repaint();
 				break;
 			}
