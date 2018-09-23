a1b4389001862cf67071273dbab146e644159301
Christoph Hart <christoph-hart@gmx.de>
2018-09-23 14:20:18 +0200

    - added build scripts

---
+++ build.sh
@@ -0,0 +1,81 @@
+# MACOS BUILD SCRIPT
+
+project=Sofia\ Woodwinds
+xmlFile=libreWinds
+build_aax=0
+build_installer=1
+
+# SET THIS TO 1 WHEN USING BUILD SERVER
+skip_build=0
+build_server=1
+code_sign=1
+
+# ENVIRONMENT VARIABLES (ALREADY SET ON BUILD SERVER)
+if (($build_server==0))
+then
+version=1.0.2
+hise_path_macos=/Volumes/Shared/Development/HISE\ modules/projects/standalone/Builds/MacOSX/build/Release/HISE.app/Contents/MacOS/HISE
+projucer_macos=/Volumes/Shared/Development/HISE\ modules/tools/projucer/Projucer.app/Contents/MacOS/Projucer
+WORKSPACE=/Volumes/Shared/Development/Projekte/dhealey_woodwinds
+PACKAGES_BUILD=/usr/local/bin/packagesbuild
+fi
+
+# STEP 1: BUILDING THE BINARIES
+# ====================================================================
+
+if (($skip_build==0))
+then
+echo $version
+
+"$hise_path_macos" set_project_folder -p:$WORKSPACE
+
+echo Making the Projucer accessible for this project
+chmod +x "$projucer_macos"
+
+echo Building the standalone app
+"$hise_path_macos" export_ci XmlPresetBackups/$xmlFile.xml -t:standalone -a:x64x86
+chmod +x "Binaries/batchCompileOSX"
+sh "Binaries/batchCompileOSX"
+
+echo Building the plugins
+"$hise_path_macos" export_ci XmlPresetBackups/$xmlFile.xml -t:instrument -p:VST_AU -a:x64x86
+chmod +x "Binaries/batchCompileOSX"
+sh "Binaries/batchCompileOSX"
+
+fi
+# STEP 2: SIGNING
+# ====================================================================
+
+vst_project=./Binaries/Builds/MacOSX/build/Release/$project.vst
+au_project=./Binaries/Builds/MacOSX/build/Release/$project.component
+standalone_project=./Binaries/Compiled/$project.app
+
+if (($code_sign==1))
+then
+echo "Signing"
+security list-keychains
+security unlock-keychain -p $keychain_password /Users/Shared/Jenkins/Library/Keychains/login.keychain
+
+echo "Signing VST & AU"
+codesign -s "$APPLE_CERTIFICATE_ID" "$au_project"
+codesign -dv --verbose=4 "$au_project"
+codesign -s "$APPLE_CERTIFICATE_ID" "$vst_project"
+codesign -dv --verbose=4 "$vst_project"
+
+echo "Signing Standalone App"
+codesign -s "$APPLE_CERTIFICATE_ID" "$standalone_project"
+codesign -dv --verbose=4 "$standalone_project"
+fi
+
+# STEP 3: BUILDING INSTALLER
+# ====================================================================
+
+if (($build_installer==1))
+then
+echo "Build Installer"
+$PACKAGES_BUILD "Installer/OSX/$project.pkgproj"
+installer_name=./$project\ $version.pkg
+productsign --sign "$APPLE_CERTIFICATE_ID_INSTALLER" "./Installer/OSX/build/$project.pkg" "$installer_name"
+else
+echo "Skip Building Installer"
+fi
